0e3707e3751a7b08445b31aee145bac9ebaa7391
qkgoalkeeper <qiankunhm@163.com>
2024-07-18 06:24:31 -0700

    [style] delete unused files

--- example/main.go
+++ example/main.go
@@ -26,25 +26,27 @@ func main() {
 		}
 	}()
 	lsm.Start(config.Config{
-		DataDir:       `E:\项目\lsm数据测试目录`,
+		DataDir:       `./example/mygo`,
 		Level0Size:    100,
 		PartSize:      4,
 		Threshold:     3000,
 		CheckInterval: 3,
 	})
+
+	insert()
 	query()
 
 }
 
 func query() {
 	start := time.Now()
-	v, _ := lsm.Get[TestValue]("aaaaaa")
+	v, _ := lsm.Get[TestValue]("aaaa")
 	elapse := time.Since(start)
 	fmt.Println("查找 aaaaaa 完成，消耗时间：", elapse)
 	fmt.Println(v)
 
 	start = time.Now()
-	v, _ = lsm.Get[TestValue]("aazzzz")
+	v, _ = lsm.Get[TestValue]("aazz")
 	elapse = time.Since(start)
 	fmt.Println("查找 aazzzz 完成，消耗时间：", elapse)
 	fmt.Println(v)
@@ -58,7 +60,7 @@ func insert() {
 		C: 3,
 		D: "00000000000000000000000000000000000000",
 	}
-	
+
 	//testVData, _ := json.Marshal(testV)
 	//// 131 个字节
 	//kvData, _ := kv.Encode(kv.Value{
@@ -74,24 +76,18 @@ func insert() {
 	//
 	count := 0
 	start := time.Now()
-	key := []byte{'a', 'a', 'a', 'a', 'a', 'a'}
+	key := []byte{'a', 'a', 'a', 'a'}
 	lsm.Set(string(key), testV)
 	for a := 0; a < 26; a++ {
 		for b := 0; b < 26; b++ {
 			for c := 0; c < 26; c++ {
 				for d := 0; d < 26; d++ {
-					for e := 0; e < 26; e++ {
-						for f := 0; f < 26; f++ {
-							key[0] = 'a' + byte(a)
-							key[1] = 'a' + byte(b)
-							key[2] = 'a' + byte(c)
-							key[3] = 'a' + byte(d)
-							key[4] = 'a' + byte(e)
-							key[5] = 'a' + byte(f)
-							lsm.Set(string(key), testV)
-							count++
-						}
-					}
+					key[0] = 'a' + byte(a)
+					key[1] = 'a' + byte(b)
+					key[2] = 'a' + byte(c)
+					key[3] = 'a' + byte(d)
+					lsm.Set(string(key), testV)
+					count++
 				}
 			}
 		}
